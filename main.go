1e58c504656a8c3911771dad3d42b1b500398569
CrimsonCrayFish <m1992steen@gmail.com>
2023-04-26 20:34:25 +0200

    added tasks and run method

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"log"
+	"os"
 	logr "ribbet_web/common/log_r"
 )
+
+// TASKS:
+//1. File store db for activities {id, description, time}
+//2. CRUD for activities
+//3. Basic Activities ui
 
 func main() {
 	l := logr.New("My Fancy Logger")
 
-	l.Println("Hello World!!!")
+	if err := run(l); err != nil {
+		l.Printf("Failed with error %s", err)
+		os.Exit(1)
+	}
+
+}
+
+func run(l *log.Logger) error {
+	l.Println("Running Application")
+
+	return nil
 }
