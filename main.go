0e70cae76cefb3504ebc5543bc97dda989036a25
CrimsonCrayFish <m1992steen@gmail.com>
2023-04-26 20:20:47 +0200

    added a logger

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
+
+const logFlags = log.Ldate | log.Ltime | log.LUTC | log.Lmsgprefix
 
 func main() {
-	fmt.Println("Hello World!!!")
+
+	l := log.New(os.Stdout, "My Fancy Logger: ", logFlags)
+
+	l.Println("Hello World!!!")
 }
